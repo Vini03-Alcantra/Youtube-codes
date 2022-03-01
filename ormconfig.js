b9e3e11aacfe4dbda2d555cc9fbcaa8862f3bcc3
Vinícius Alcântra <viniciusalcantarabarros@gmail.com>
2022-03-01 14:53:11 -0300

    Change ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,9 @@
+const ssl = "?ssl=true";
+const urlConnetcion = process.env.DATABASE_URL+ssl;  
+
 module.exports = {
   "type": "postgres",
-  "url": process.env.DATABASE_URL,   
+  "url": urlConnetcion,   
   "entities": [
     "dist/models/**/*.js"
  ],
